0ac09d6a261dd7cfaeb7249b327af38b9612f4d5
Patrick Boettcher <patrick.boettcher@posteo.de>
2014-12-27 19:38:52 +0100

    footer: adapt text

--- footer.php
+++ footer.php
@@ -14,9 +14,12 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<?php do_action( 'sundance_credits' ); ?>
-			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'sundance' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'sundance' ), 'WordPress' ); ?></a>
+
+			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'sundance' ); ?>" rel="generator"><?php printf( __( '%s', 'sundance' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'sundance' ), 'Sundance', '<a href="https://wordpress.com/themes/" rel="designer">WordPress.com</a>' ); ?>
+			<?php printf( __( 'Theme %1$s by %2$s.', 'sundance' ), 'Sundance', '<a href="https://wordpress.com/themes/" rel="designer">WordPress.com</a>' ); ?>
+			<span class="sep"> | </span>
+			with modifcations made by <a href="https://github.com/pboettch/sundance-pb" target="_blank">me</a>
 		</div><!-- .site-info -->
 	</footer><!-- .site-footer .site-footer -->
 </div><!-- #page .hfeed .site -->
